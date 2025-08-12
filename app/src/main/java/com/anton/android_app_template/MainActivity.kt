94b053e19313c431573d65ab0bbccb348c890135
Anton Ber <holzed15@gmail.com>
2025-08-12 13:28:21 +0300

    работа с методами SharedPreferences

--- app/src/main/java/com/anton/android_app_template/MainActivity.kt
+++ app/src/main/java/com/anton/android_app_template/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.anton.android_app_template
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,13 +10,43 @@ import com.anton.android_app_template.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private val prefsName = "my_prefs"
+    private val keyName = "user_name"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
-        
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        ///data/data/com.anton.android_app_template/shared_prefs/my_prefs.xml
+//        String
+//
+
+//        Boolean
+//
+//        Float
+//
+//        Long
+//
+//        Set<String>
+        val prefs = getSharedPreferences(prefsName, Context.MODE_PRIVATE)
+        //        Int
+        prefs.edit().putInt("int_key", 42).apply()
+        //        Boolean
+        prefs.edit().putBoolean("boolean_key", true).apply()
+        //        Set<String>
+        prefs.edit().putStringSet("set_key", setOf<String>("test","test1","test2")).apply()
+        val savedName = prefs.getString(keyName, null)
+        if (savedName != null) {
+            binding.textViewResult.text = "$savedName"
+        }
+        binding.buttonSave.setOnClickListener {
+            val name = binding.editTextName.text.toString()
+            if (name.isNotEmpty()) {
+                prefs.edit().putString(keyName,name).apply()
+                binding.textViewResult.text = name
+            }
+        }
     }
 }
