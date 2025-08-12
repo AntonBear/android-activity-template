c1bbfa78b2a846431e08df4ab7ab8d0a2e9fb55e
Anton Ber <holzed15@gmail.com>
2025-08-12 16:41:03 +0300

    array list  practise

---
+++ app/src/main/java/com/anton/android_app_template/Array.java
@@ -0,0 +1,8 @@
+package com.anton.android_app_template;
+
+import java.util.ArrayList;
+
+public class Array {
+    int[] array = new int[1];
+    ArrayList<Integer> list = new ArrayList<>(5);
+}
--- app/src/main/java/com/anton/android_app_template/MainActivity.kt
+++ app/src/main/java/com/anton/android_app_template/MainActivity.kt
@@ -14,5 +14,46 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // List
+        val fruits: List<String> = listOf("test","test2","test3")
+        val firstFruit = fruits[0]
+//        fruits.add() -> ошибка
+
+        // mutableListOf creates ArrayList under the hood
+        val mutableFruit: MutableList<String> = mutableListOf("test")
+        mutableFruit.add("test")
+        mutableFruit[0] = "test"
+
+
+        val arrayList: ArrayList<String> = arrayListOf<String>("test","test2")
+        arrayList.add("test")
+        arrayList[0] = "test"
+
+        val array: Array<String> = arrayOf("test")
+//        array.add() -> ошибка
+        array[0] = "test"
+
+
+
+        // | Тип           | Можно изменять? | Методы изменения                            |
+        //| ------------- | --------------- | ------------------------------------------- |
+        //| `List`        | Нет             | Нет (`add()`, `remove()` нет)               |
+        //| `MutableList` | Да              | Есть (`add()`, `remove()`, `clear()` и др.) |
+
+
+        val allFruits = StringBuilder()
+        for (fruit in fruits) {
+            allFruits.append(fruit).append("\n")
+        }
+        binding.textViewResult.text = allFruits.toString()
+
+        val hasTest = "test" in fruits // true
+
+        //Изменяемый список
+        val mutableFruits = fruits.toMutableList()
+        mutableFruits.add("test3")
+
     }
+
 }
