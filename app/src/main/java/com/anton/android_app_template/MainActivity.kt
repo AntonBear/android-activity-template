a81fbfdf83f236fbaf3151390d23a9bb7c66d3d2
Anton Ber <holzed15@gmail.com>
2025-08-12 09:48:44 +0300

    initial commit
    # Conflicts:
    #	app/build.gradle.kts
    #	app/src/main/java/com/anton/android_app_template/MainActivity.kt

--- app/src/main/java/com/anton/android_app_template/MainActivity.kt
+++ app/src/main/java/com/anton/android_app_template/MainActivity.kt
@@ -5,13 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.anton.android_app_template.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+
         setContentView(R.layout.activity_main)
     }
 }
