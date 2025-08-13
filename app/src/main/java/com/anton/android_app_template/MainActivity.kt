29240e54e9b036a76feb347b59c2de568f899678
Anton Ber <holzed15@gmail.com>
2025-08-13 14:35:09 +0300

    добавление всевозможных логов + понижение версии навигации

--- app/src/main/java/com/anton/android_app_template/MainActivity.kt
+++ app/src/main/java/com/anton/android_app_template/MainActivity.kt
@@ -5,11 +5,66 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.anton.android_app_template.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Получаем NavController из NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Логируем при каждом изменении экрана
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val destId = destination.id
+            val destName = try {
+                resources.getResourceEntryName(destId) // вернёт имя из nav_graph
+            } catch (e: Exception) {
+                "unknown"
+            }
+            Log.d("NavController", "Текущий экран: $destName")
+            logNavControllerStackCompat()
+            logFragmentManager()
+        }
     }
+
+    // Логирование FragmentManager
+    private fun logFragmentManager() {
+        val fragments = supportFragmentManager.fragments.map { it::class.java.simpleName }
+        Log.d("FragmentManagerState", "Fragments в SupportFragmentManager: $fragments")
+
+        // Также можно глянуть внутренние фрагменты в NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val childFragments = navHostFragment.childFragmentManager.fragments.map { it::class.java.simpleName }
+        Log.d("FragmentManagerState", "Fragments внутри NavHostFragment: $childFragments")
+    }
+
+    // Логирование стека NavController
+    private fun logNavControllerStackCompat() {
+        val stackStrings = navController.backQueue.map { entry ->
+            val name = entry.destination.label?.toString() ?: try {
+                resources.getResourceEntryName(entry.destination.id)
+            } catch (e: Exception) {
+                "unknown"
+            }
+            name
+        }
+
+        Log.d("NavBackStack", "NavController BackStack: $stackStrings")
+    }
+
 }
