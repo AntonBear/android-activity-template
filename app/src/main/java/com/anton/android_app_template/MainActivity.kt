df87c3dcf889e74df1278dab8b85e93e5cd8ee8f
Anton Ber <holzed15@gmail.com>
2025-08-13 10:47:52 +0300

    Один экран жизненный цикл

--- app/src/main/java/com/anton/android_app_template/MainActivity.kt
+++ app/src/main/java/com/anton/android_app_template/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.anton.android_app_template
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.anton.android_app_template.databinding.ActivityMainBinding
@@ -14,5 +15,43 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        updateStatus("onCreate()")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        updateStatus("onStart()")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateStatus("onResume()")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        updateStatus("onPause()")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        updateStatus("onStop()")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        updateStatus("onRestart()")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        updateStatus("onDestroy()")
+    }
+
+    private fun updateStatus(method: String) {
+        binding.statusText.text = "Текущий метод: $method"
+        Log.d("Жизненный цикл", "$method")
+        println("Жизненный цикл: $method") // Логи в Logcat
     }
 }
